6b93ab44f115e5531f2053f2d0b5697dbec451f0
rejaul-karim10 <rejalkarim66666@gmail.com>
2022-11-29 14:46:04 +0800

    added save user to databse api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const jwt = require('jsonwebtoken')
+const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
@@ -19,42 +19,82 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verifyJWT(req, res, next) {
+  // console.log('token inside JWT', req.headers.authorization);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+  const token = authHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
+
 async function run() {
   try {
-    const categoriesCollection = client.db("resellUsedLaptop").collection('categories')
-    const usersCollection = client.db("resellUsedLaptop").collection('users')
-
+    const categoriesCollection = client
+      .db("resellUsedLaptop")
+      .collection("categories");
+    const usersCollection = client.db("resellUsedLaptop").collection("users");
+    const productsCollection = client
+      .db("resellUsedLaptop")
+      .collection("products");
 
     // save user email and generate jwt
-    app.put('/user/:email', async(req, res)=>{
-      const email = req.params.email
-      const user = req.body
-      const filter = {email:email}
-      const options = {upsert: true}
-      const updatedDoc={
-        $set: user,
-      }
-      const result = usersCollection.updateOne(filter,updatedDoc,options)
+    // app.put("/user/:email", async (req, res) => {
+    //   const email = req.params.email;
+    //   const user = req.body;
+    //   const filter = { email: email };
+    //   const options = { upsert: true };
+    //   const updatedDoc = {
+    //     $set: user,
+    //   };
+    //   const result = await usersCollection.updateOne(
+    //     filter,
+    //     updatedDoc,
+    //     options
+    //   );
 
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{
-        expiresIn: '1d'
-      })
-      res.send({result, token})
-    })
+    //   const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    //     expiresIn: "1d",
+    //   });
+    //   console.log(token);
+    //   res.send({ result, token });
+    // });
 
-
-    app.get('/categories', async(req, res)=>{
-      const query = {}
-      const categories = await categoriesCollection.find(query).toArray()
-      res.send(categories)
-    })
-
-    app.get("/categories/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const category = await categoriesCollection.findOne(query);
-      res.send(category);
+    app.get("/product-categories", async (req, res) => {
+      const query = {};
+      const categories = await categoriesCollection.find(query).toArray();
+      res.send(categories);
     });
+
+    app.get('/product/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = {_id: ObjectId(id)}
+      const cursor = productsCollection.find(query );
+      const selectedProduct = await cursor.toArray()
+      res.send(selectedProduct);
+  });
+  
+    app.get('/category/:id', async (req, res) => {
+      const id = req.params.id;
+      const cursor = productsCollection.filter(p=>p.category_id === id );
+      const category_Product = await cursor.toArray()
+      res.send(category_Product);
+  });
+
+  // save user to database
+  app.post('/users', async(req, res)=>{
+    const user = req.body;
+    const result = await usersCollection.insertOne(user)
+    res.send(result)
+  })
 
   } finally {
   }
